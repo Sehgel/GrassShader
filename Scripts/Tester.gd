4f138350a5a099b3ccb9fea8c18ccbf04f584549
Sehgel <lucasemanuelgonzalez8@gmail.com>
2020-02-26 23:01:44 -0300

    Initial commit

---
+++ Scripts/Tester.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(sin(0))
+	print(sin(1))
+	print(sin(2))
+	print(sin(PI))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
